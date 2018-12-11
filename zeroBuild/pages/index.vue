9b6a7ea28e786b2d5d5da1cd9b959b5427dbaaf2
tskon <tsekhmeister@petshop.ru>
2018-12-11 12:50:42 +0300

    create init app state

--- zeroBuild/pages/index.vue
+++ zeroBuild/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Nuxt from zero</h1>
+    <div id="vueApp">
+        <h1>{{ title }}</h1>
+        <main>some text111</main>
+    </div>
 </template>
+
+<script>
+
+    export default {
+        data: ()=> {return {
+            title: 'Nuxt from zero'
+        }},
+    };
+</script>
