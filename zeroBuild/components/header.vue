d441a242fa908d938973ef42c31e2c9c5433a4c0
tskon <tsekhmeister@petshop.ru>
2018-12-11 13:03:48 +0300

    add header component

---
+++ zeroBuild/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h1>Nuxt test app</h1>
+        <p>text from slot: <slot></slot></p>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "commonHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- zeroBuild/pages/index.vue
+++ zeroBuild/pages/index.vue
@@ -1,15 +1,23 @@
 <template>
     <div id="vueApp">
+        <common-header>Текст хедера</common-header>
         <h1>{{ title }}</h1>
         <main>some text111</main>
     </div>
 </template>
 
 <script>
+    import CommonHeader from '~/components/header.vue';
 
     export default {
-        data: ()=> {return {
-            title: 'Nuxt from zero'
-        }},
+        data() {
+            return {
+                title: 'Nuxt from zero'
+            }
+        },
+
+        components: {
+            CommonHeader
+        }
     };
 </script>
